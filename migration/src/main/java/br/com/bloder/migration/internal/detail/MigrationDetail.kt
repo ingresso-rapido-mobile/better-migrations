13d3d621b94226ab4ad1cfedba4c05946d147acb
Daniel <wowmegadenis@hotmail.com>
2016-09-24 20:09:05 -0300

    Create migration detail class

---
+++ migration/src/main/java/br/com/bloder/migration/internal/detail/MigrationDetail.kt
@@ -0,0 +1,12 @@
+package br.com.bloder.migration.internal.detail
+
+import br.com.bloder.migration.Migration
+
+/**
+ * Created by bloder on 24/09/16.
+ */
+class MigrationDetail {
+
+    var migration : Migration? = null
+    var version : Int = 0
+}
