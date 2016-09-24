28e45f1cefd342fbb02d34bdc1a4442b26b254f0
Daniel <wowmegadenis@hotmail.com>
2016-09-24 20:08:06 -0300

    Create migration class

---
+++ migration/src/main/java/br/com/bloder/migration/Migration.kt
@@ -0,0 +1,13 @@
+package br.com.bloder.migration
+
+import br.com.bloder.migration.internal.InternalMigration
+
+/**
+ * Created by bloder on 24/09/16.
+ */
+class Migration : InternalMigration {
+
+    override fun up() {}
+    override fun down() {}
+    override fun change() {}
+}
