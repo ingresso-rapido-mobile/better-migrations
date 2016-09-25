01277d1b5edb27cb0c211942ae567ede418a4867
Daniel <wowmegadenis@hotmail.com>
2016-09-25 00:11:49 -0300

    Create up and down functions logic

--- migration/src/main/java/br/com/bloder/migration/BetterMigration.kt
+++ migration/src/main/java/br/com/bloder/migration/BetterMigration.kt
@@ -1,15 +1,24 @@
 package br.com.bloder.migration
 
-import br.com.bloder.migration.internal.InternalMigration
+import br.com.bloder.migration.internal.detail.MigrationDetail
 import io.realm.DynamicRealm
 import io.realm.RealmMigration
 
 /**
- * Created by bloder on 24/09/16.
+ * Abstract better migration actions to transform in
+ * a realm migration
  */
-class BetterMigration(internalMigration: InternalMigration) : RealmMigration {
+class BetterMigration(private val migration: MigrationDetail) : RealmMigration {
 
+    /**
+     * Realm Migration main function
+     */
     override fun migrate(realm: DynamicRealm?, oldVersion: Long, newVersion: Long) {
-
+        if (newVersion < oldVersion) {
+            migration.migration?.down()
+        }
+        if (newVersion == migration.version) {
+            migration.migration?.up()
+        }
     }
 }
