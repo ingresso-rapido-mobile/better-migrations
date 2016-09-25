d32d2b990dd42b6700340a08e60a8fa60f57f917
Daniel <wowmegadenis@hotmail.com>
2016-09-25 00:12:29 -0300

    Remove change function and document functions

--- migration/src/main/java/br/com/bloder/migration/Migration.kt
+++ migration/src/main/java/br/com/bloder/migration/Migration.kt
@@ -3,11 +3,18 @@ package br.com.bloder.migration
 import br.com.bloder.migration.internal.InternalMigration
 
 /**
- * Created by bloder on 24/09/16.
+ * Class to abstract realm migration
  */
 class Migration : InternalMigration {
 
+    /**
+     * Represents what will change in a migration version
+     */
     override fun up() {}
+
+    /**
+     * Represents what will be discarded when that migration version
+     * does not exist anymore
+     */
     override fun down() {}
-    override fun change() {}
 }
