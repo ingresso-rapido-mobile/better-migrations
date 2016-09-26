3bf6914b1481c03d207cbf4de5c44bfe1458651d
Daniel <wowmegadenis@hotmail.com>
2016-09-26 01:48:23 -0300

    Call realm config creation function

--- app/src/main/java/br/com/bloder/better_migrations/MainActivity.java
+++ app/src/main/java/br/com/bloder/better_migrations/MainActivity.java
@@ -3,11 +3,15 @@ package br.com.bloder.better_migrations;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import br.com.bloder.better_migrations.realm.internal.RealmConfig;
+import io.realm.RealmConfiguration;
+
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    RealmConfiguration realmConfiguration = new RealmConfig(this).configRealm();
   }
 }
