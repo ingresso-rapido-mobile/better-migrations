12a079f3ed576b22766ed09da0516eaea2e6e67e
Daniel <wowmegadenis@hotmail.com>
2016-09-26 01:49:30 -0300

    Set dynamic realm object as param in migration

--- migration/src/main/java/br/com/bloder/migration/Migration.kt
+++ migration/src/main/java/br/com/bloder/migration/Migration.kt
@@ -1,20 +1,21 @@
 package br.com.bloder.migration
 
 import br.com.bloder.migration.internal.InternalMigration
+import io.realm.DynamicRealm
 
 /**
  * Class to abstract realm migration
  */
-class Migration : InternalMigration {
+open class Migration : InternalMigration {
 
     /**
      * Represents what will change in a migration version
      */
-    override fun up() {}
+    override fun up(realm: DynamicRealm?) {}
 
     /**
      * Represents what will be discarded when that migration version
      * does not exist anymore
      */
-    override fun down() {}
+    override fun down(realm: DynamicRealm?) {}
 }
