4671532793b6510c68dd180a718f0204379d00ee
Bloder <aptgetinstalldanielleme@gmail.com>
2016-09-30 15:40:43 -0300

    Check realm action in schema details class

--- migration/src/main/java/br/com/bloder/migration/schema/BetterSchema.kt
+++ migration/src/main/java/br/com/bloder/migration/schema/BetterSchema.kt
@@ -4,6 +4,7 @@ import io.realm.DynamicRealm
 import io.realm.RealmObjectSchema
 import io.realm.RealmSchema
 
+
 /**
  * Created by bloder on 28/09/16.
  */
@@ -11,19 +12,22 @@ import io.realm.RealmSchema
 class BetterSchema {
 
     private var schema: RealmSchema? = null
+    private val add: String = "add"
 
     fun getSchema(dynamicRealm: DynamicRealm) : RealmSchema {
         this.schema = dynamicRealm.schema
         return schema as RealmSchema
     }
 
-    fun addField(name: String, type: Class<*>) : SchemaDetails = SchemaDetails(schema as RealmSchema, name, type)
+    fun addField(name: String, type: Class<*>) : SchemaDetails = SchemaDetails(schema as RealmSchema, name, type, add)
 
-    class SchemaDetails(private var schema: RealmSchema, private val name: String, private val type: Class<*>) {
+    class SchemaDetails(private var schema: RealmSchema, private val name: String, private val type: Class<*>, private val func: String) {
 
         fun into(className: String) {
             val realmObjectSchema: RealmObjectSchema = schema.get(className)
-            if (!realmObjectSchema.hasField(className)) realmObjectSchema.addField(name, type)
+            when(func.toUpperCase()) {
+                SCHEMA_ACTIONS.ADD.name -> if (!realmObjectSchema.hasField(className)) realmObjectSchema.addField(name, type)
+            }
         }
     }
 }
