c0bb6d0543c819aaee995f59e9d2f3448b67d0bd
Bloder <aptgetinstalldanielleme@gmail.com>
2016-09-30 17:45:13 -0300

    Add remove function in better schema

--- migration/src/main/java/br/com/bloder/migration/schema/BetterSchema.kt
+++ migration/src/main/java/br/com/bloder/migration/schema/BetterSchema.kt
@@ -1,5 +1,6 @@
 package br.com.bloder.migration.schema
 
+import br.com.bloder.migration.schema.SCHEMA_ACTIONS.*
 import io.realm.DynamicRealm
 import io.realm.RealmObjectSchema
 import io.realm.RealmSchema
@@ -12,21 +13,24 @@ import io.realm.RealmSchema
 class BetterSchema {
 
     private var schema: RealmSchema? = null
-    private val add: String = "add"
 
     fun getSchema(dynamicRealm: DynamicRealm) : RealmSchema {
         this.schema = dynamicRealm.schema
         return schema as RealmSchema
     }
 
-    fun addField(name: String, type: Class<*>) : SchemaDetails = SchemaDetails(schema as RealmSchema, name, type, add)
+    fun addField(name: String, type: Class<*>) : SchemaDetails = SchemaDetails(schema as RealmSchema, name, type, ADD)
+    fun removeField(name: String) : SchemaDetails = SchemaDetails(schema as RealmSchema, name, REMOVE)
 
-    class SchemaDetails(private var schema: RealmSchema, private val name: String, private val type: Class<*>, private val func: String) {
+    class SchemaDetails(private var schema: RealmSchema, private val name: String, private val type: Class<*>?, private val func: SCHEMA_ACTIONS) {
+
+        constructor(schema: RealmSchema, name: String, func: SCHEMA_ACTIONS) : this(schema, name, null, func) {}
 
         fun into(className: String) {
             val realmObjectSchema: RealmObjectSchema = schema.get(className)
-            when(func.toUpperCase()) {
-                SCHEMA_ACTIONS.ADD.name -> if (!realmObjectSchema.hasField(className)) realmObjectSchema.addField(name, type)
+            when(func) {
+                ADD -> if (!realmObjectSchema.hasField(className)) realmObjectSchema.addField(name, type)
+                REMOVE -> if (realmObjectSchema.hasField(name)) realmObjectSchema.removeField(name)
             }
         }
     }
