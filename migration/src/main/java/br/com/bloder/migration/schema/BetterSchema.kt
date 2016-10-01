795ab6a8138267cf99eb56965c97011239ac1e74
Daniel <wowmegadenis@hotmail.com>
2016-10-01 11:31:10 -0300

    Create change type function

--- migration/src/main/java/br/com/bloder/migration/schema/BetterSchema.kt
+++ migration/src/main/java/br/com/bloder/migration/schema/BetterSchema.kt
@@ -2,8 +2,10 @@ package br.com.bloder.migration.schema
 
 import br.com.bloder.migration.schema.SCHEMA_ACTIONS.*
 import io.realm.DynamicRealm
+import io.realm.DynamicRealmObject
 import io.realm.RealmObjectSchema
 import io.realm.RealmSchema
+import java.util.*
 
 
 /**
@@ -19,19 +21,33 @@ class BetterSchema {
         return schema as RealmSchema
     }
 
-    fun addField(name: String, type: Class<*>) : SchemaDetails = SchemaDetails(schema as RealmSchema, name, type, ADD)
+    fun addField(name: String, type: Class<*>) : SchemaDetails = SchemaDetails(schema as RealmSchema, name, type, null, ADD)
     fun removeField(name: String) : SchemaDetails = SchemaDetails(schema as RealmSchema, name, REMOVE)
+    fun changeFieldType(name: String, oldType: Class<*>, newType: Class<*>) : SchemaDetails = SchemaDetails(schema as RealmSchema, name, newType, oldType, CHANGE_TYPE)
 
-    class SchemaDetails(private var schema: RealmSchema, private val name: String, private val type: Class<*>?, private val func: SCHEMA_ACTIONS) {
+    class SchemaDetails(private var schema: RealmSchema, private val name: String, private val type: Class<*>?, private val oldType: Class<*>?, private val func: SCHEMA_ACTIONS) {
 
-        constructor(schema: RealmSchema, name: String, func: SCHEMA_ACTIONS) : this(schema, name, null, func) {}
+        constructor(schema: RealmSchema, name: String, func: SCHEMA_ACTIONS) : this(schema, name, null, null, func) {}
 
         fun into(className: String) {
             val realmObjectSchema: RealmObjectSchema = schema.get(className)
             when(func) {
                 ADD -> if (!realmObjectSchema.hasField(className)) realmObjectSchema.addField(name, type)
                 REMOVE -> if (realmObjectSchema.hasField(name)) realmObjectSchema.removeField(name)
+                CHANGE_TYPE -> changeType(realmObjectSchema, name, type as Class<*>, oldType as Class<*>)
             }
+        }
+
+        private fun changeType(realmObjectSchema: RealmObjectSchema, name: String, newType: Class<*>, oldType: Class<*>) {
+            val randomExtension: String = name + UUID.randomUUID().toString()
+            realmObjectSchema.addField(randomExtension, newType)
+            realmObjectSchema.transform(RealmObjectSchema.Function { obj -> obj.setLong(randomExtension, obj.getInt(name).toLong()) })
+            realmObjectSchema.removeField(name)
+            realmObjectSchema.renameField(randomExtension, name)
+        }
+
+        private fun transformValue() {
+
         }
     }
 }
